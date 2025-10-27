625b957cf1d0c39f3fc984410ced91e1887a9bed
Teo Miscia <teomiscia@Teos-MacBook-Pro.local>
2025-10-26 19:53:46 -0700

    Fix theme choice

--- web/resources/views/layouts/app.blade.php
+++ web/resources/views/layouts/app.blade.php
@@ -1,110 +1,69 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-        <script>
-            if (document.cookie.includes('theme=dark')) {
-                document.documentElement.classList.add('dark');
-            } else if (!document.cookie.includes('theme=') && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                document.documentElement.classList.add('dark');
-            } else {
-                document.documentElement.classList.remove('dark');
-            }
-        </script>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @livewireStyles
+    <!-- Styles -->
+    @vite('resources/css/app.css')
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <script>
+        if (document.cookie.includes('theme=dark')) {
+            document.documentElement.classList.add('dark');
+        } else if (!document.cookie.includes('theme=') && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    </script>
+</head>
+<body class="bg-gray-900 text-white">
+    <nav class="p-6 bg-gray-800 flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="/" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="/dashboard" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="/business-plans" class="p-3">Business Plans</a>
+            </li>
+        </ul>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+        <ul class="flex items-center">
+            @auth
+                <li>
+                    <a href="" class="p-3">{{ auth()->user()->name }}</a>
+                </li>
+                <li>
+                    <form action="{{ route('logout') }}" method="post" class="p-3 inline">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
+            @endauth
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-        <script>
-            function copyToClipboard(elementId) {
-                var copyText = document.getElementById(elementId);
-                copyText.select();
-                copyText.setSelectionRange(0, 99999); /* For mobile devices */
-                document.execCommand("copy");
-                alert("Copied the link: " + copyText.value);
-            }
-
-            function enterPresentationMode() {
-                const element = document.documentElement; // Get the root HTML element
-                if (element.requestFullscreen) {
-                    element.requestFullscreen();
-                } else if (element.mozRequestFullScreen) { /* Firefox */
-                    element.mozRequestFullScreen();
-                } else if (element.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-                    element.webkitRequestFullscreen();
-                } else if (element.msRequestFullscreen) { /* IE/Edge */
-                    element.msRequestFullscreen();
-                }
-
-                // Optionally, hide navigation or other UI elements in presentation mode
-                document.body.classList.add('presentation-mode');
-            }
-
-            function exitPresentationMode() {
-                if (document.exitFullscreen) {
-                    document.exitFullscreen();
-                } else if (document.mozCancelFullScreen) { /* Firefox */
-                    document.mozCancelFullScreen();
-                } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-                    document.webkitExitFullscreen();
-                } else if (document.msExitFullscreen) { /* IE/Edge */
-                    document.msExitFullscreen();
-                }
-
-                document.body.classList.remove('presentation-mode');
-            }
-
-            // Listen for fullscreen change events to adjust UI
-            document.addEventListener('fullscreenchange', () => {
-                if (!document.fullscreenElement) {
-                    exitPresentationMode();
-                }
-            });
-            document.addEventListener('mozfullscreenchange', () => {
-                if (!document.mozFullScreenElement) {
-                    exitPresentationMode();
-                }
-            });
-            document.addEventListener('webkitfullscreenchange', () => {
-                if (!document.webkitFullscreenElement) {
-                    exitPresentationMode();
-                }
-            });
-            document.addEventListener('msfullscreenchange', () => {
-                if (!document.msFullscreenElement) {
-                    exitPresentationMode();
-                }
-            });
-        </script>
-        @livewireScripts
-    </body>
+            @guest
+                <li>
+                    <a href="{{ route('login') }}" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
+        </ul>
+    </nav>
+    @yield('content')
+</body>
 </html>
