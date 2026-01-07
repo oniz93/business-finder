babb2eb717849a29ffad575437eb7eda0ecf7edc
oniz93 <teo.miscia@gmail.com>
2026-01-07 17:03:17 +0000

    Composer updates and mail services

--- web/config/mail.php
+++ web/config/mail.php
@@ -97,6 +97,7 @@ return [
             'retry_after' => 60,
         ],
         'mailgun' => [
+            'transport' => 'mailgun',
             'domain' => env('MAILGUN_DOMAIN'),
             'secret' => env('MAILGUN_SECRET'),
             'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
--- web/config/services.php
+++ web/config/services.php
@@ -52,5 +52,11 @@ return [
         'client_secret' => env('TWITTER_CLIENT_SECRET'),
         'redirect' => env('TWITTER_REDIRECT_URI'),
     ],
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
+    ],
 
 ];
