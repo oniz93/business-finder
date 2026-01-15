e512160eda7a3a63f2589a4b734973a8f0580182
Teo Miscia <teo.miscia@gmail.com>
2026-01-15 08:30:02 -0800

    Added possibility to delete account if not accepting privacy policy

--- web/resources/views/auth/privacy-consent.blade.php
+++ web/resources/views/auth/privacy-consent.blade.php
@@ -31,7 +31,15 @@
             <x-input-error :messages="$errors->get('receives_product_updates')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-between mt-8">
+        <div class="flex items-center justify-end mt-8">
+            <x-primary-button>
+                {{ __('Accept and Continue') }}
+            </x-primary-button>
+        </div>
+    </form>
+
+    <div class="mt-10 pt-6 border-t border-gray-200 dark:border-gray-700">
+        <div class="flex items-center justify-between">
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <button type="submit" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
@@ -39,9 +47,52 @@
                 </button>
             </form>
 
-            <x-primary-button class="ms-4">
-                {{ __('Accept and Continue') }}
-            </x-primary-button>
+            <button 
+                x-data=""
+                x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
+                class="text-sm text-red-600 dark:text-red-400 hover:text-red-900 dark:hover:text-red-300 underline font-medium"
+            >
+                {{ __('Delete Account') }}
+            </button>
         </div>
-    </form>
+    </div>
+
+    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+        <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
+            @csrf
+            @method('delete')
+
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                {{ __('Are you sure you want to delete your account?') }}
+            </h2>
+
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                {{ __('If you do not accept the Privacy Policy, you can delete your account. All of your resources and data will be permanently deleted. Please enter your password to confirm.') }}
+            </p>
+
+            <div class="mt-6">
+                <x-input-label for="delete_password" value="{{ __('Password') }}" class="sr-only" />
+
+                <x-text-input
+                    id="delete_password"
+                    name="password"
+                    type="password"
+                    class="mt-1 block w-3/4"
+                    placeholder="{{ __('Password') }}"
+                />
+
+                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+            </div>
+
+            <div class="mt-6 flex justify-end">
+                <x-secondary-button x-on:click="$dispatch('close')">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+
+                <x-danger-button class="ms-3">
+                    {{ __('Delete Account') }}
+                </x-danger-button>
+            </div>
+        </form>
+    </x-modal>
 </x-guest-layout>
